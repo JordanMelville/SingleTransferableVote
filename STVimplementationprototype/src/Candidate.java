1cd25ffac81a8b99541faa17a40c38a2b910d388
Jordan Melville <pqb12175@ds.strath.ac.uk>
2017-02-06 16:27:13 +0000

    Managed to fix up a few issues with the vote reader such as taking into account preferences. At the moment it calculates the number of votes for each candidate properly, but uses static arraylists, i am looking for a way to sort this at the moment.

--- STVimplementationprototype/src/Candidate.java
+++ STVimplementationprototype/src/Candidate.java
@@ -1,39 +1,30 @@
+import java.util.ArrayList;
 
 // This class has been designed to take into account all of the party data, these calculations are used in calculating the final seats
 public class Candidate {
 
 
 private String candidateName;
-private int firstpreference;
-private int secondpreference;
-private int totalSeats;
+private ArrayList<Integer> totalVotes;
 
-public Candidate(String name, int firstPreference, int secondPreference) {
+public Candidate(String name) {
 	candidateName = name;
-	firstpreference = firstPreference;
-	secondpreference = secondPreference;
+	totalVotes = new ArrayList<Integer>(); 
 }
 
-public void addSeat() {
-	totalSeats += 1;
-}
 
 // formula for working out the threshold for winning a seat
 
-public int firstPreference() {
-	return firstpreference;
+public void addVote(Integer vote) {
+	totalVotes.add(vote);
 }
-
 
 public String candidateName() {
 	return candidateName;
 }
 
 
-public int totalSeats() {
-	return totalSeats;
-	
-}
+
 
 
 }
--- STVimplementationprototype/src/STVImplementation.java
+++ STVimplementationprototype/src/STVImplementation.java
@@ -14,7 +14,15 @@ public class STVImplementation {
 	
 private ArrayList<Candidate> candidateNames = new ArrayList<Candidate>();
 private ArrayList<Integer> votes = new ArrayList<Integer>();
+private ArrayList<Integer> secondPreferences = new ArrayList<Integer>();
+private ArrayList<Integer> orange = new ArrayList<Integer>();
+private ArrayList<Integer> pear = new ArrayList<Integer>();
+private ArrayList<Integer> chocolate = new ArrayList<Integer>();
+private ArrayList<Integer> strawberries = new ArrayList<Integer>();
+private ArrayList<Integer> sweets = new ArrayList<Integer>();
+
 private int seats = 3;
+HashMap<String, Integer> candidateData = new HashMap<String, Integer>();
 
 public void addCandidate(Candidate candidate) {
 	candidateNames.add(candidate);
@@ -50,39 +58,121 @@ public int droopQuota() {
 }
 
 // load in data and create party data to be then used for calculating seats
+
+// here i list a number of issues i need to fix
+// 1. get the vote reader working. vote reader works fine - uses static arraylists at the moment(bad)
+// 2. get the quota working with votes read in. - quota works fine at the moment
+// 3. Make sure round 1 of the system works properly.
+// 4. get a highest and lowest quota function to work out who is eliminated at the end of a round
+// 5. Properly calculate full rounds of the wikipedia game
+// 6. Adapt to work for all inputs
+
 public void LoadVotingData(String file) throws FileNotFoundException {
 	Scanner sc;
+	int voteCounter = 0;
+
+	
 	try{
 		
 		sc = new Scanner(new File(file));
 		
-		int totalCandidates = sc.nextInt();
-		int totalVotes = sc.nextInt();
-		sc.nextLine();
-		
-		System.out.println("The total number of candidates in the vote are as follows: " + totalCandidates);
-		System.out.println("The total number of votes cast in the election are as follows : " + totalVotes);
-		
-		for(int i = 0; i < totalVotes; i++) {
-			votes.add(i);
-		}
-		
-		System.out.println("the quota calculated to win a seat in the first round is " + droopQuota());
-		
-
-		while(sc.hasNext()) {
-			String candidateName = sc.next();
-			int firstPreference = sc.nextInt();
-			int secondPreference = sc.nextInt();
-			Candidate newCandidate = new Candidate(candidateName, firstPreference, secondPreference);
-			this.addCandidate(newCandidate);
-			System.out.println("Candidates name: " + candidateName);
-			System.out.println("First preference vote for the candidate " + firstPreference);
-			System.out.println("Second preference vote for the candidate " + secondPreference);
-		
-		}
 	
+		while(sc.hasNext()) {
+			String vote = sc.next();
+			if (vote.length() > 1){
+				char[] preferences = vote.toCharArray();
+				int firstPreference = Character.getNumericValue(preferences[0]);
+				System.out.println("first preference " + firstPreference);
+				int secondPreference = Character.getNumericValue(preferences[1]);
+				System.out.println("second preference " + secondPreference);
+				System.out.println("");
+				voteCounter++;
+				if(firstPreference == 1) {
+					orange.add(firstPreference);
+				}
+				if(firstPreference == 2) {
+					pear.add(firstPreference);
+				}
+				if(firstPreference == 3) {
+					chocolate.add(firstPreference);
+				}
+				if(firstPreference == 4) {
+					strawberries.add(firstPreference);
+				}
+				if(firstPreference == 5) {
+					sweets.add(firstPreference);
+				}
+				votes.add(firstPreference);
+				secondPreferences.add(secondPreference);
+				
+			}
+			// if the input does not contain more than 1 preference we then 
+			else {
+				char[] noprefs = vote.toCharArray();
+				int originalVote = Character.getNumericValue(noprefs[0]);
+				System.out.println("first preference " + originalVote);
+				System.out.println("");
+				voteCounter++;
+                if(originalVote == 1) {
+					orange.add(originalVote);
+				}
+				if(originalVote == 2) {
+					pear.add(originalVote);
+				}
+				if(originalVote == 3) {
+					chocolate.add(originalVote);
+					
+				}
+				if(originalVote == 4) {
+					strawberries.add(originalVote);
+				}
+				if(originalVote == 5) {
+					sweets.add(originalVote);
+				}
+				votes.add(originalVote);
+				
+			}
 			
+		}
+		System.out.println("The droop quota is as follows " + droopQuota());
+		System.out.println("The number of votes conducted for oranges: " + orange.size());
+		System.out.println("The number of votes conducted for pears: " + pear.size());
+		System.out.println("The number of votes conducted for chocolate: " + chocolate.size());
+		System.out.println("The number of votes conducted for strawberries: " + strawberries.size());
+		System.out.println("The number of votes conducted for sweets: " + sweets.size());
+		
+		
+		if(orange.size() > droopQuota()) {
+			System.out.println("Orange has been elected in round 1");
+		}
+		else {
+			System.out.println("Orange has not met the quota.");
+		}
+		if(pear.size() > droopQuota()) {
+			System.out.println("Pear has been elected in round 1");
+		}
+		else {
+			System.out.println("Pear has not met the quota.");
+		}
+		if(chocolate.size() > droopQuota()) {
+			System.out.println("Chocolate has been elected in round 1");	
+		}
+		else {
+			System.out.println("Chocolate has not met the quota.");
+		}
+		if(strawberries.size() > droopQuota()) {
+			System.out.println("Strawberries has been elected in round 1");
+		} else
+		{
+			System.out.println("Strawberries has not met the quota. ");
+		}
+		if(sweets.size() > droopQuota()) {
+			System.out.println("Sweets has been elected in round 1");
+		} else {
+			System.out.println("Sweets has not met the quota");
+		}
+			
+		
 		} catch (FileNotFoundException ex) {
 			System.out.println(ex);
 	} 
